e4b29333166bc535143a4dbd5c20e33c091a08d1
lajafar <142849075+lajafar@users.noreply.github.com>
2023-09-10 18:47:56 -0400

    added files

---
+++ index.js
@@ -0,0 +1,41 @@
+
+let numH = 0
+countHome = document.getElementById("counthome")
+countHome.textContent = 0
+
+function addOneH() {
+    numH += 1
+    countHome.textContent = numH
+}
+
+function addTwoH() {
+    numH += 2
+    countHome.textContent = numH
+}
+
+function addThreeH() {
+    numH += 3
+    countHome.textContent = numH
+}
+
+let numG = 0
+countGuest = document.getElementById("countguest")
+countGuest.textContent = 0
+
+function addOne() {
+    numG += 1
+    countGuest.textContent = numG
+}
+
+function addTwo() {
+    numG += 2
+    countGuest.textContent = numG
+}
+
+function addThree() {
+    numG += 3
+    countGuest.textContent = numG
+}
+
+// window.addOne = addOne
+
